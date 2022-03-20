60ff9fb74095e9a7776988eddb5e1f403061926f
vit225 <vitaly1090@mail.ru>
2022-03-21 09:33:37 +1000

    исправил ошибки

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,38 +1,39 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
-import java.sql.*;
+
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDaoJDBCImpl userDaoJDBCImpl = new UserDaoJDBCImpl();
+    UserDao userDao = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
-            userDaoJDBCImpl.createUsersTable();
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        userDaoJDBCImpl.dropUsersTable();
+        userDao.dropUsersTable();
     }
 
-    public void saveUser(String name, String lastName, byte age)  {
-        userDaoJDBCImpl.saveUser(name, lastName, age);
+    public void saveUser(String name, String lastName, byte age) {
+        userDao.saveUser(name, lastName, age);
 
     }
 
     public void removeUserById(long id) {
-        userDaoJDBCImpl.removeUserById(id);
-        }
+        userDao.removeUserById(id);
+    }
 
 
     public List<User> getAllUsers() {
-       return userDaoJDBCImpl.getAllUsers();
+        return userDao.getAllUsers();
     }
 
 
     public void cleanUsersTable() {
-        userDaoJDBCImpl.cleanUsersTable();
+        userDao.cleanUsersTable();
     }
 }
