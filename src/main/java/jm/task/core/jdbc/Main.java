8b053fa84b7e2f6a59aa517a67469fd00c160b46
vit225 <vitaly1090@mail.ru>
2022-03-21 23:35:55 +1000

    jk

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,12 +1,6 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,15 +9,15 @@ public class Main {
 
         userService.createUsersTable();
 
-        userService.saveUser("Иван","Иванов",(byte) 25);
-        userService.saveUser("Петя","Петров",(byte) 30);
-        userService.saveUser("Сергей","Сергеев",(byte) 35);
-        userService.saveUser("Николай","Николаев",(byte) 31);
+        userService.saveUser("Иван", "Иванов", (byte) 25);
+        userService.saveUser("Петя", "Петров", (byte) 30);
+        userService.saveUser("Сергей", "Сергеев", (byte) 35);
+        userService.saveUser("Николай", "Николаев", (byte) 31);
 
         System.out.println(userService.getAllUsers().toString());
 
         userService.cleanUsersTable();
 
-         userService.dropUsersTable();
+        //userService.dropUsersTable();
     }
 }
